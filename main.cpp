23178c2ae49982170199a4334e7d18f0e58b9c9a
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 15:11:00 +0300

    Millioner task
    - if x2
    - cout x4
    - ">" x2

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+   int main()
+   {
+    unsigned  int a ;
+    cout<<"Vasha zarplata($): ";
+    cin >>a;
+    if (a>1000)
+    {
+        if (a>1000000){
+        cout <<"Ti millioner!";
+        goto mark;
+        }
+        cout <<"Horosho";
+        goto mark;
+    }
+    cout <<"Rabotai bolshe";
+    mark:;
+    cout <<"...no ti molodec";
+    return 0;
+   }
