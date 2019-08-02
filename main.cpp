39f09b23f03fabd21d5b1f0f37246edc2deb1bfc
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 20:31:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main()
 {
-   unsigned  int a ;
-   cout<<"Vasha zarplata($): ";
-   cin >>a;
-   if (a>1000)
+    unsigned  int a ;
+    cout<<"Vasha zarplata($): ";
+    cin >>a;
+    if (a>1000)
     {
-      if (a>1000000)
+        if (a>1000000)
         {
-          cout <<"Ti millioner!";
-          goto mark;
+            cout <<"Ti millioner!";
+            goto mark;
         }
-      cout <<"Horosho";
-      goto mark;
+        cout <<"Horosho";
+        goto mark;
     }
     cout <<"Rabotai bolshe";
     mark:;
